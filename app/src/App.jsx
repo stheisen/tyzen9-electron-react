ccd18930beb8738ba793c3333760c45cad1ce4de
Steve Theisen <stevetheisen@gmail.com>
2023-02-22 20:46:09 -0500

    Moved to HashRouter

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
 
 // React component
 function App() {
+  // BrowserRouter is not well supported in packaged Electron apps, therfore "HashRouter"
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
