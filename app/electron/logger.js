5d20847621de0df95617eed30e0007c797b44bd5
Steve Theisen <stevetheisen@gmail.com>
2023-02-22 20:45:09 -0500

    Logger location based on environment

--- app/electron/logger.js
+++ app/electron/logger.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-shadow */
 const { createLogger, format, transports } = require('winston');
 const path = require('path');
 const { app } = require('electron');
 require('winston-daily-rotate-file');
 
 const { timestamp, label, printf } = format;
+const isDev = process.env.NODE_ENV === 'development';
 
 // Winston supports the following 6 log levels
 // error:   0
@@ -14,16 +16,21 @@ const { timestamp, label, printf } = format;
 // debug:   5
 // silly:   6
 
-// This is where we will write the file logs to
-const logPath = path.resolve(app.getPath('userData'), 'Logs');
+// The environment will determine the log file path
+let logPath;
+if (isDev) {
+  logPath = path.resolve(__dirname, '../../logs');
+} else {
+  logPath = path.resolve(app.getPath('userData'), 'Logs');
+}
 
 // Create the logFormat for the console
-const consoleLogFormat = printf(({ level, message, timedate }) => `${timedate} ${level}: ${message}`);
+const consoleLogFormat = printf(({ level, message, timestamp }) => `${timestamp} ${level}: ${message}`);
 
 // Create the logFormat for the rotating log files
-const logfileFormat = printf(({ timedate, level, message }) => {
+const logfileFormat = printf(({ timestamp, level, message }) => {
   const messageVal = message.replace(/[""]/g, '\\"');
-  return `{"timestamp":"${timedate}","level":"${level}","message":"${messageVal}"}`;
+  return `{"timestamp":"${timestamp}","level":"${level}","message":"${messageVal}"}`;
 });
 
 // Configure logging to a file
@@ -55,6 +62,6 @@ const consoleTransport = new transports.Console({
 // Enable the file logger, and add the console loader if we are in developmet mode.
 const logger = createLogger();
 logger.add(fileTransport);
-if (process.env.NODE_ENV === 'development') logger.add(consoleTransport);
+if (isDev) logger.add(consoleTransport);
 
 module.exports = logger;
